b26e2dabbc66179d0781c995e2d2a1f88a3b02d0
xiaoxue chen <xiaoxue.beijing@gmail.com>
2017-02-06 12:08:21 +0000

    add basic tests

---
+++ test/ApplicationTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.*;
+import org.junit.Test;
+import play.mvc.Result;
+import play.test.WithApplication;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.routeAndCall;
+
+
+/**
+ * Basic Tests
+ */
+public class ApplicationTest extends WithApplication{
+
+    @Test
+    public void simpleCheck() {
+        int a = 1 + 1;
+        assertEquals(a,2);
+
+    }
+
+    @Test
+    public void testCallIndex() {
+        Result result = route(
+                fakeRequest()
+        );
+        assertEquals(OK, result.status());
+    }
+
+}
