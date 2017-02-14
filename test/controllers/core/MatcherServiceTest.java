5356886a16294611a03ad00f20cb5c9e126dd286
Vuong Nguyen <vuongnq.09@gmail.com>
2017-02-14 15:46:25 +0000

    Refactor test code

---
+++ test/controllers/core/MatcherServiceTest.java
@@ -0,0 +1,26 @@
+package controllers.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by vuongnguyen on 14/02/2017.
+ */
+public class MatcherServiceTest {
+    @Test
+    public void testGetAgreedUsers() {
+        assertEquals("First assert", 1, 1);
+    }
+
+    @Test
+    public void testGetWaitingUsers() {
+        assertEquals("First assert", 1, 1);
+    }
+
+    @Test
+    public void testGetMatcherUsers() {
+        assertEquals("First assert", 1, 1);
+    }
+}
