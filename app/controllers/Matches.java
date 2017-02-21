f71701cbe96c2b7a38d447e9002b997f3150b363
xiaoxue chen <xiaoxue.beijing@gmail.com>
2017-02-21 15:57:04 +0000

    add matches view and controller

---
+++ app/controllers/Matches.java
@@ -0,0 +1,101 @@
+package controllers;
+
+import models.User;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static play.libs.Json.toJson;
+
+/**
+ * Matches page controller
+ */
+
+@Security.Authenticated(Secured.class)
+public class Matches extends Controller {
+
+
+    public Result index() {
+        return ok(views.html.matches.render(User.findByEmail(request().username())));
+    }
+
+    //TODO
+    public Result getMatchedList() {
+        List<User> matchedUsers = new ArrayList<>();
+        //fake data
+        User fake1 = new User();
+        fake1.fullname = "Fake One";
+        fake1.email = "fake1@gmail.com";
+        fake1.id = 101L;
+
+        User fake2 = new User();
+        fake2.fullname = "Fake Two";
+        fake2.email = "fake2@gmail.com";
+        fake2.id = 102L;
+
+        matchedUsers.add(0, fake1);
+        matchedUsers.add(1, fake2);
+        return ok(toJson(matchedUsers));
+    }
+
+    //TODO
+    public Result getWaitingList() {
+        List<User> waitingReplyFromUsers = new ArrayList<>();
+        //fake data
+        User fake3 = new User();
+        fake3.fullname = "Fake Three";
+        fake3.email = "fake3@gmail.com";
+        fake3.id = 103L;
+
+        User fake4 = new User();
+        fake4.fullname = "Fake Four";
+        fake4.email = "fake4@gmail.com";
+        fake4.id = 104L;
+        waitingReplyFromUsers.add(0, fake3);
+        waitingReplyFromUsers.add(1, fake4);
+
+        return ok(toJson(waitingReplyFromUsers));
+    }
+
+    //TODO
+    public Result getNewSuggestionMatches() {
+        List<User> newMatches = new ArrayList<>();
+        //fake data
+        User fake5 = new User();
+        fake5.fullname = "Fake Five";
+        fake5.email = "fake5@gmail.com";
+        fake5.id = 105L;
+        User fake6 = new User();
+        fake6.fullname = "Fake Six";
+        fake6.email = "fake6@gmail.com";
+        fake6.id = 106L;
+        newMatches.add(0, fake5);
+        newMatches.add(1, fake6);
+
+        User fake7 = new User();
+        fake7.fullname = "Fake Seven";
+        fake7.email = "fake7@gmail.com";
+        fake7.id = 107L;
+        User fake8 = new User();
+        fake8.fullname = "Fake Eight";
+        fake8.email = "fake8@gmail.com";
+        fake8.id = 108L;
+        User fake9 = new User();
+        fake9.fullname = "Fake Nine";
+        fake9.email = "fake9@gmail.com";
+        fake9.id = 109L;
+        User fake10 = new User();
+        fake10.fullname = "Fake Ten";
+        fake10.email = "fake10@gmail.com";
+        fake10.id = 110L;
+        newMatches.add(2, fake7);
+        newMatches.add(3, fake8);
+        newMatches.add(4, fake9);
+        newMatches.add(5, fake10);
+        return ok(toJson(newMatches));
+    }
+}
+
