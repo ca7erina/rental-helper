81197e5ecdfdf6834794a6133127075c85be3c46
Vuong Nguyen <vuongnq.09@gmail.com>
2017-04-02 16:56:49 +0100

    Try to fix travis CI error 255

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 # Pull new code and re-build docker image
-cd ~/code/rental-helper && git pull --rebase
-cd ~/code/rental-helper && sbt docker:publishLocal
+#cd /home/vuongnq.09/code/rental-helper && git pull --rebase
+#cd /home/vuongnq.09/code/rental-helper && sbt docker:publishLocal
 
 # Tag image and push to Docker Hub
-docker tag rental-helper:1.0-SNAPSHOT vn09/rental-helper:1.0-SNAPSHOT
-docker push vn09/rental-helper:1.0-SNAPSHOT
+#docker tag rental-helper:1.0-SNAPSHOT vn09/rental-helper:1.0-SNAPSHOT
+#docker push vn09/rental-helper:1.0-SNAPSHOT
 
 # Pull new image and update the service
-docker pull vn09/rental-helper:1.0-SNAPSHOT
-docker service update --force rental-helper
-docker service scale rental-helper=4
+#docker pull vn09/rental-helper:1.0-SNAPSHOT
+#docker service update --force rental-helper
+#docker service scale rental-helper=4
+
+echo "Hello world"
